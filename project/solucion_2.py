eb2c2a4d3179f30806233194b479df6eadb558eb
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-10-16 21:48:37 +0200

    project improved

--- project/solucion_2.py
+++ project/solucion_2.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import pymysql.cursors
 import random
 import codecs
+import sys
 
 def retrieve_riddles():
   query = "SELECT texto, solucion FROM adivinanzas"
@@ -30,10 +32,29 @@ def retrieve_riddles():
 
 riddles = retrieve_riddles()
 
+num_intentos = 10
+
+print 
+print "==========================="
+print "El Juego de las Adivinanzas"
+print "==========================="
+print "Introduza 'quit' para salir"
+print 
+
 while True:
   rand = random.randint(0, len(riddles)-1) 
   print codecs.encode(riddles[rand][0], 'utf-8')
-  answer = raw_input()
-  if answer == riddles[rand][1]:
-    print "respuesta correcta"
-    break
+  for intento in range(num_intentos):
+      answer = raw_input()
+      if answer == riddles[rand][1]:
+        print "Respuesta correcta. A ver si puedes con la siguiente:"
+        break
+      elif answer == "quit":
+        print "Final del juego. ¡Hasta pronto!"
+        sys.exit()
+      else:
+        if intento + 1 < num_intentos:
+          print "Respuesta incorrecta. Vuelve a intentarlo, te quedan %d intentos" % (num_intentos - (intento + 1))
+        else:
+          print "Máximo número de intentos. Probemos con otra adivinanza"
+      
