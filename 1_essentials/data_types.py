a10fb9a1a63aafcdb054cea149d78de52897394b
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-10-17 15:33:18 +0200

    project layout modified and multiple assignment example added

---
+++ 1_essentials/data_types.py
@@ -0,0 +1,21 @@
+# Integer
+a = 4
+print a
+print type(a)
+# Float
+b = 5.6
+print b
+print type(b)
+# String
+c = "hello"
+print c
+print type(c)
+
+# Multiple
+d, e = 5, "6"
+
+print d
+print type(d)
+print e
+print type(e)
+
--- essentials/data_types.py
+++
@@ -1,9 +0,0 @@
-# Integer
-a = 4
-print type(a)
-# Float
-b = 5.6
-print type(b)
-# String
-c = "hello"
-print type(c)
