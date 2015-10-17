1fdc2799b081f45deb7980cd182556bc869777ea
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-10-17 13:16:14 +0200

    use of the statistics package from standard library

---
+++ stdlib/python_3/stats.py
@@ -0,0 +1,10 @@
+import statistics
+
+random_variable = [2.75, 2.75, 2, 3, 5, 4.2]
+print("Media")
+print(statistics.mean(random_variable))
+print("Mediana") 
+print(statistics.median(random_variable))
+print("Varianza")
+print(statistics.variance(random_variable))
+
