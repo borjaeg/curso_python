888e2850811230c2c29402c26698000747116d62
Borja A. Espejo-Garcia <borja3790@gmail.com>
2015-10-18 15:50:08 +0200

    scraping example added

---
+++ 10_scraping/picasso_scraping.py
@@ -0,0 +1,20 @@
+import urllib2
+from bs4 import BeautifulSoup
+import time
+
+url = 'https://es.wikipedia.org/wiki/Pablo_Picasso'
+
+html = urllib2.urlopen(url)
+soup = BeautifulSoup(html, 'html.parser')
+
+imgs = soup.findAll('img', src = True)
+for img in imgs:
+  print img['src']
+  print img['alt']
+  name = img['src'].split('/')[-1]
+  print name
+  image = open(name, "wb")
+  download_img = urllib2.urlopen('https:' + img['src'])
+  image.write(download_img.read())
+  image.close()
+  time.sleep(1)
